fbab6d81d6fec055ff6fed6be66ec6c458cb7e0e
Oliver Schmid <oliver.schmid@ebrains.eu>
2022-03-24 21:21:16 +0100

    add target URL for KG in message

--- hello_kg/hello_kg.py
+++ hello_kg/hello_kg.py
@@ -1,12 +1,13 @@
 from kg_core.kg import KGv3
 
 class HelloKG():
-    def __init__(self):
+    def __init__(self, kg:KGv3):
+        self.kg = kg
         pass
 
     def say_hello(self, properties):
-        print(f"Hello {properties['name']}! Welcome to the KG automation!")
+        print(f"Hello {properties['name']}! Welcome to the KG automation! I'm going to talk to {self.kg.endpoint}")
 
 
 def run(properties:dict, kg:KGv3):
-    HelloKG().say_hello(properties)
+    HelloKG(kg).say_hello(properties)
