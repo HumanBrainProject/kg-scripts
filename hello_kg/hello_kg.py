59713526a6a29c1a7cb25eb2fb80e73123e32610
Oliver Schmid <oliver.schmid@ebrains.eu>
2022-03-24 21:42:32 +0100

    add user info to check if the authentication is correct

--- hello_kg/hello_kg.py
+++ hello_kg/hello_kg.py
@@ -1,12 +1,14 @@
 from kg_core.kg import KGv3
 
+
 class HelloKG():
     def __init__(self, kg:KGv3):
         self.kg = kg
         pass
 
     def say_hello(self, properties):
-        print(f"Hello {properties['name']}! Welcome to the KG automation! I'm going to talk to {self.kg.endpoint}")
+        me = self.kg.me()
+        print(f"Hello {properties['name']} - aka {me['http://schema.org/name']}! Welcome to the KG automation! I'm going to talk to {self.kg.endpoint}")
 
 
 def run(properties:dict, kg:KGv3):
